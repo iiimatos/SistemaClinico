0aa579cdfbea094bc0320dde80e7fb314a46193b
ivan__emv12 <ivanmatosvillar@gmail.com>
2020-03-30 17:37:44 -0400

    Controlador de citas medicas creado y funciona con exito

---
+++ ProyectoFinal/ProyectoFinal/Controllers/Procesos/TCitasController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal.Models;
+using ProyectoFinal.Models.Tablas;
+
+namespace ProyectoFinal.Controllers.Procesos
+{
+    public class TCitasController : Controller
+    {
+        private ClinicaContext db = new ClinicaContext();
+
+        // GET: TCitas
+        public ActionResult Index()
+        {
+            var citas = db.Citas.Include(t => t.Medicos).Include(t => t.Pacientes);
+            return View(citas.ToList());
+        }
+
+
+        // GET: TCitas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Medicos_Registrado = new SelectList(db.Medicos, "IdMedicos", "Nombre");
+            ViewBag.Pacientes_Registrado = new SelectList(db.Pacientes, "IdPacientes", "Nombre");
+            return View();
+        }
+
+        // POST: TCitas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCitas,Pacientes_Registrado,Fecha_Reserva,Hora_Reserva,Medicos_Registrado")] TCitas tCitas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Citas.Add(tCitas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Medicos_Registrado = new SelectList(db.Medicos, "IdMedicos", "Nombre", tCitas.Medicos_Registrado);
+            ViewBag.Pacientes_Registrado = new SelectList(db.Pacientes, "IdPacientes", "Nombre", tCitas.Pacientes_Registrado);
+            return View(tCitas);
+        }
+
+    }
+}

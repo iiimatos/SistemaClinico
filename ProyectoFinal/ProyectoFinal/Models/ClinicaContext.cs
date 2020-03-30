cceed4dc2f1742384f209076d5741246cdcb86d9
ivan__emv12 <ivanmatosvillar@gmail.com>
2020-03-30 00:34:52 -0400

    Modelo context creado y archivo we.confi configurado

---
+++ ProyectoFinal/ProyectoFinal/Models/ClinicaContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ProyectoFinal.Models.Tablas;
+
+namespace ProyectoFinal.Models
+{
+    public class ClinicaContext:DbContext
+    {
+        public ClinicaContext() : base("ClinicaDataBase") { }
+
+        public DbSet<Pacientes> Pacientes { get; set; }
+        public DbSet<Habitaciones> Habitaciones { get; set; }
+        public DbSet<Medicos> Medicos { get; set; }
+        public DbSet<TCitas> Citas { get; set; }
+        public DbSet<TIngresos> Ingresos { get; set; }
+        public DbSet<TAltaMedica> AltaMedicas { get; set; }
+
+    }
+}
